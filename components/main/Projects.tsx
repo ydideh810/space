2957c8a046b6e18de3c262002976e631017fab0c
ydideh810 <yd.baller810@gmail.com>
2024-08-29 19:40:03 -0400

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -7,7 +7,7 @@ export const Projects = () => {
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard src="/nidai.png" title="Your ultimate private assistant!" description="With over 30 LLMs in store, you can chat, generate, summerize and automate in any way you want whilst being in total control of your data. At all times. 
 " />
-        <ProjectCard src="/NidLabs.png" title="Your private, open-source, AI assistant platform." description="Tune to empower your team with AI." />
+        <ProjectCard src="/Nidlabs.png" title="Your private, open-source, AI assistant platform." description="Tune to empower your team with AI." />
         <ProjectCard src="/nisearch.png" title="Decentralized AI Search Engine." description="For a healthier internet." />
       </div>
     </div>
