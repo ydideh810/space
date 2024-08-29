e8964f2d22fb7a78319e9b5305a5e5f59fed7c94
ydideh810 <yd.baller810@gmail.com>
2024-08-29 16:33:33 -0400

    Update Encription.tsx

--- components/main/Encription.tsx
+++ components/main/Encription.tsx
@@ -10,18 +10,18 @@ const Encription = () => {
       <div className="absolute w-auto h-auto top-0 z-[5]">
         <motion.div className="text-[40px] font-medium text-center text-gray-200" variants={slideInFromTop}>
           AI
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500"> & </span>
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-stone-500"> & </span>
           Security
         </motion.div>
       </div>
 
       <div className="flex flex-col items-center justify-center translate-y-[-50px] absolute z-[20] w-auto h-auto">
         <div className="flex flex-col group items-center cursor-pointer w-auto h-auto">
-          <Image className="translate-y-5 transition-all duration-200 group-hover:translate-y-11" src="/LockTop.png" alt="Lock Top" width={50} height={50} />
-          <Image className="z-10" src="/LockMain.png" alt="Lock Main" width={70} height={70} />
+          <Image className="translate-y-5 transition-all duration-200 group-hover:translate-y-11" src="/Top.png" alt="Lock Top" width={50} height={50} />
+          <Image className="z-10" src="/Lock.png" alt="Lock Main" width={70} height={70} />
         </div>
 
-        <div className="Welcome-box px-[15px] py-[4px] z-[20] border my-[20px] border-[#7042f88b] opacity-[0.9]">
+        <div className="Welcome-box px-[15px] py-[4px] z-[20] border my-[20px] border-[#B20000] opacity-[0.9]">
           <h1 className="Welcome-text text-[12px]">Encription</h1>
         </div>
       </div>
