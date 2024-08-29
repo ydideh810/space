778b099e56c469cc40908c63c83e52b6b73152be
ydideh810 <yd.baller810@gmail.com>
2024-08-28 23:07:41 -0400

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -15,17 +15,17 @@ export const HeroContent = () => {
 
         <motion.div variants={slideInFromLeft(0.5)} className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto">
           <span>
-            Providing
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500"> the best </span>
-            project experience
+            Experience
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500"> the power of AI</span>
+            without compromising your privacy.
           </span>
         </motion.div>
 
         <motion.p variants={slideInFromLeft(0.8)} className="text-lg text-gray-400 my-5 max-w-[600px]">
-          I&apos;m a Full Stack Engineer with experience in Websites, Mobile, and Software development. Check out my projects and skills.
+          Niddam is an AI company dedicated to providing you with LLM products & tools that enhance your workflow & respects your privacy!
         </motion.p>
         <motion.a variants={slideInFromLeft(1)} className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]">
-          Learn more!
+          EXPLORE
         </motion.a>
       </div>
 
