0eaf709d12d6b3efd7f424f657603e98459f01c6
ydideh810 <yd.baller810@gmail.com>
2024-08-29 21:26:06 -0400

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -9,14 +9,14 @@ export const HeroContent = () => {
     <motion.div initial="hidden" animate="visible" className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]">
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]" variants={slideInFromTop}>
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">Fullstack Developer Portfolio</h1>
+          <SparklesIcon className="text-[#FF0000] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">Ultimate Private AI Enterprise</h1>
         </motion.div>
 
         <motion.div variants={slideInFromLeft(0.5)} className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto">
           <span>
             Experience
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500"> the power of AI</span>
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-900 to-cyan-500"> the power of AI</span>
             without compromising your privacy.
           </span>
         </motion.div>
