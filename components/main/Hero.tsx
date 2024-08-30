8d62530655edbed0a0f0552ec64dd7877c9adc8c
ydideh810 <yd.baller810@gmail.com>
2024-08-30 16:40:24 -0400

    Update Hero.tsx

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,7 +1,7 @@
 import { HeroContent } from "@/components/sub/HeroContent";
 export const Hero = () => {
   return (
-    <div className="relative flex  flex-col h-full shadow-[#03001417]/50 bg-[#610E0F] w-full"> 
+    <div className="relative flex flex-col h-full shadow-[#03001417]/50 bg-[rgba(97,14,15,0.32)] w-full">
       <video autoPlay muted loop className="rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover">
         <source src="/void.mp4" type="video/mp4" />
       </video>
