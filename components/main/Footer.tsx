0ab684dee2bb1cf1edcb8736fa489202e15b33a0
ydideh810 <yd.baller810@gmail.com>
2024-08-29 21:14:51 -0400

    Update Footer.tsx

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -4,7 +4,49 @@ export const Footer = () => {
     <footer className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px]">
       <div className="w-full flex flex-col items-center justify-center m-auto">
         <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
-          
+         
+          {/*  <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <FaYoutube />
+              <span className="text-[15px] ml-[6px]">Youtube</span>
+            </p>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <RxGithubLogo />
+              <span className="text-[15px] ml-[6px]">Github</span>
+            </p>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <RxDiscordLogo />
+              <span className="text-[15px] ml-[6px]">Discord</span>
+            </p>
+          </div>
+          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+            <div className="font-bold text-[16px]">Social Media</div>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <RxInstagramLogo />
+              <span className="text-[15px] ml-[6px]">Instagram</span>
+            </p>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <RxTwitterLogo />
+              <span className="text-[15px] ml-[6px]">Twitter</span>
+            </p>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <RxLinkedinLogo />
+              <span className="text-[15px] ml-[6px]">Linkedin</span>
+            </p>
+          </div>
+          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+            <div className="font-bold text-[16px]">About</div>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <span className="text-[15px] ml-[6px]">Become Sponsor</span>
+            </p>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <span className="text-[15px] ml-[6px]">Learning about me</span>
+            </p>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <span className="text-[15px] ml-[6px]">krayton.1235@gmail.com</span>
+            </p>
+          </div>
+        </div> */}
+
          
         
 
