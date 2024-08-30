03f29546de22718c66dea68b81aed03bbeefbfe4
ydideh810 <yd.baller810@gmail.com>
2024-08-29 21:19:34 -0400

    Update Footer.tsx

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
   return (
     <footer className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px]">
       <div className="w-full flex flex-col items-center justify-center m-auto">
-        <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
+      {/*  <div className="w-full h-full flex flex-row items-center justify-around flex-wrap"> */}
          
           {/*  <p className="flex flex-row items-center my-[15px] cursor-pointer">
               <FaYoutube />
@@ -51,7 +51,7 @@ export const Footer = () => {
         
 
         <div className="mb-[20px] text-[15px] text-center">Niddam &copy; All right reserved</div>
-      </div>
+        </div>
     </footer>
   );
 };
