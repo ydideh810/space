b7cd8acb4f2c20e241b2ef60b57279558438aca9
ydideh810 <yd.baller810@gmail.com>
2024-08-29 22:43:52 -0400

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -3,7 +3,7 @@ import { ProjectCard } from "../sub/ProjectCard";
 export const Projects = () => {
   return (
     <div className="flex flex-col items-center justify-center py-20">
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-red-900 py-20">My Projects</h1>
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-red-900 py-20">P R O D U C T S</h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard src="/nidai.png" title="Your ultimate private assistant!" description="With over 30 LLMs in store, you can chat, generate, summerize and automate in any way you want whilst being in total control of your data. At all times. 
 " />
