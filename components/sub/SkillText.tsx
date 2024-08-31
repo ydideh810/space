d3e85abdfd03ec2a973692b52f2a459d153ce56e
ydideh810 <yd.baller810@gmail.com>
2024-08-31 00:09:03 -0400

    Update SkillText.tsx

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -7,14 +7,14 @@ const SkillText = () => {
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center">
       <motion.div className="Welcome-box py-[8px] px-[7px] border border-[#633232] opacity-[0.9]" variants={slideInFromTop}>
-        <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+        <SparklesIcon className="text-[#633232] mr-[10px] h-5 w-5" />
         <h1 className="Welcome-text text-[13px]">Work better with Niddam</h1>
       </motion.div>
-      <motion.div variants={slideInFromLeft(0.5)} className="text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]">
+      <motion.div variants={slideInFromLeft(0.5)} className="text-[30px] text-white font-medium text-gray-100 mt-[10px] text-center mb-[15px]">
         Bringing you the world of LLM 
       </motion.div>
-      <motion.div variants={slideInFromRight(0.5)} className="cursive text-[20px] text-gray-200 bg-10 mt-[10px] text-center">
-        Without breaking the bank
+      <motion.div variants={slideInFromRight(0.5)} className="cursive text-[20px] text-gray-100 bg-10 mt-[10px] text-center">
+        Without entering your personal details
       </motion.div>
     </div>
   );
