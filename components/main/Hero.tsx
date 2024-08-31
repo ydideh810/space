49c9819501366f4878514d82ec7e7c873dfc3843
ydideh810 <yd.baller810@gmail.com>
2024-08-30 21:12:58 -0400

    Update Hero.tsx

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -3,7 +3,7 @@ export const Hero = () => {
   return (
     <div className="relative flex flex-col h-full w-full">
       <video autoPlay muted loop className="rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover">
-        <source src="/blackhole.webm" type="video/webm" />
+        <source src="/void.mp4" type="video/mp4" />
       </video>
       <HeroContent />
     </div>
