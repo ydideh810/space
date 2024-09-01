310f3903ef958d330002de1e5fe6cb3753b5d1e8
ydideh810 <yd.baller810@gmail.com>
2024-08-31 23:21:23 -0400

    Update Skills.tsx

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,4 +1,4 @@
-import { Frontend_skill,  Other_skill } from "@/constants";
+import { Frontend_skill } from "@/constants";
 import SkillDataProvider from "../sub/SkillDataProvider";
 import SkillText from "../sub/SkillText";
 
@@ -11,12 +11,7 @@ const Skills = () => {
           <SkillDataProvider key={index} src={image.Image} width={image.width} height={image.height} index={index} />
         ))}
       </div>
-      <div className="flex flex-row justify-arround flex-wrap mt-4 gap-5 items-center">
-        {Other_skill.map((image, index) => (
-          <SkillDataProvider key={index} src={image.Image} width={image.width} height={image.height} index={index} />
-        ))}
-      </div>
-
+      
       <div className="w-full h-full absolute">
         <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
           <video className="w-full h-auto" preload="false" playsInline loop muted autoPlay src="/drop.mp4" />
