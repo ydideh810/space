4ed1bb64d1f8c63a311cd7a91d77da14cad31b16
ydideh810 <yd.baller810@gmail.com>
2024-09-01 23:26:21 -0400

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -5,6 +5,7 @@ export const Projects = () => {
     <div className="flex flex-col items-center justify-center py-20">
       <h1 className="text-[40px] font-semibold text-stone-50 bg-clip-text py-20">P R O D U C T S</h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+        
         <a href="https://www.niddampro.site/">
         <ProjectCard src="/nidai.png" title="Your ultimate private assistant!" description="With over 30 LLMs in store, you can chat, generate, summerize and automate in any way you want whilst being in total control of your data. At all times. 
 " /> 
@@ -13,7 +14,7 @@ export const Projects = () => {
         <ProjectCard src="/Nidlabs.png" title="Your private, open-source, AI assistant platform." description="Tune to empower your team with AI." />
         
     
-        <ProjectCard src="/nisearch.png" title="Decentralized AI Search Engine." description="For a healthier internet." href="https://findto-eight.vercel.app/en" />
+        <ProjectCard src="/nisearch.png" title="Decentralized AI Search Engine." description="For a healthier internet." />
            
       </div>
     </div>
